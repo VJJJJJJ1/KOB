d17f964ab8f81dc97fd3bbb7ee84e12653b5201a
VJJJJJJ1 <1607945383@qq.com>
2022-09-25 10:53:07 +0800

    项目骨架搭建好了

--- web/src/App.vue
+++ web/src/App.vue
@@ -1,10 +1,43 @@
 <template>
   <div>
-    hello world
+    <div> Bot 昵称：{{ bot_name }}</div>
+    <div> Bot 战力：{{ bot_rating }}</div>
   </div>
   <router-view/>
 </template>
 
-<style>
+<script>
 
+import { ref } from "vue";
+import $ from "jquery";
+
+export default{
+  name:"App",
+  setup: () =>{
+    let bot_name = ref("");
+    let bot_rating = ref("");
+
+    $.ajax({
+      url:"http://localhost:3000/pk/getBotinfo/",
+      type: "get",
+      success: resp => {
+        // console.log(resp);
+        bot_name.value = resp.name;
+        bot_rating.value = resp.rating;
+      }
+    });
+
+    return{
+      bot_name,
+      bot_rating
+    }
+  }
+}
+</script>
+
+<style>
+body{
+  background-image: url("@/assets/background.png");
+  background-size : cover; 
+}
 </style>
