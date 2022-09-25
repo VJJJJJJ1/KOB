a5a25026f1310521710539fd1ad2ce8f0873121a
VJJJJJJ1 <1607945383@qq.com>
2022-09-25 10:02:49 +0800

    更新前后端，创建getBotinfo

--- backend/src/main/java/com/kob/backend/controller/pk/BotInfoController.java
+++ backend/src/main/java/com/kob/backend/controller/pk/BotInfoController.java
@@ -4,11 +4,22 @@ package com.kob.backend.controller.pk;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.*;
+
 @RestController
 @RequestMapping("/pk/")
 public class BotInfoController {
     @RequestMapping("/getBotinfo/")
-    public String getBotInfo(){
-        return "hhhh";
+    public List<Map<String, String>> getBotInfo(){
+        List<Map<String, String>> list = new LinkedList<>();
+        Map<String, String> bot1 = new HashMap<>();
+        bot1.put("name", "tiger");
+        bot1.put("rating", "1500");
+        Map<String, String> bot2 = new HashMap<>();
+        bot2.put("name", "apple");
+        bot2.put("rating", "1800");
+        list.add(bot1);
+        list.add(bot2);
+        return list;
     }
 }
